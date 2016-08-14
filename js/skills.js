50e04558570635e8d9010a1baad8cf2dfc816d91
Marco A. Peyrot <mpeyrotc@andrew.cmu.edu>
2016-08-13 22:59:59 -0500

    Add skills to site
    Summary:
    All the skills are shown in a big circle in which the description
    is shown in the center. JS needed because it has a fixed width.
    
    Correct images still missing and content also.

---
+++ js/skills.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    var getSkillElement = function(obj) {
+        skill = $(obj).data('skillname');
+        return $('#' + skill);
+    };
+        
+    imgs = $('.skill-line img');
+    imgs.mouseover(function() {
+        getSkillElement(this).addClass('active-skill');
+    });
+    
+    imgs.mouseout(function() {
+        getSkillElement(this).removeClass('active-skill');
+    });
+});
