d2de5e0b9162cefc555ae9bb7a9c6d5f26500948
Marco Peyrot <mpeyrotc@Marcos-MacBook-Pro-2.local>
2017-06-23 20:04:52 -0500

    Restore previous version and update

---
+++ js/gallery.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+    //Handles the carousel thumbnails
+    $('[id^=carousel-selector-]').click(function () {
+        var id_selector = $(this).attr("id");
+        try {
+            var id = /-(\d+)$/.exec(id_selector)[1];
+            console.log(id_selector, id);
+            jQuery('#myCarousel').carousel(parseInt(id));
+        } catch (e) {
+            console.log('Regex failed!', e);
+        }
+    });
+});
